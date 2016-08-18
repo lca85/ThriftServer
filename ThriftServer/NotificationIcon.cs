e23e800aa0d3a3ba01f329afec24cfa0270ce86e
Lisbey Collazo <lcalfonso85@gmail.com>
2016-08-18 14:56:41 -0700

    Initial commit

---
+++ ThriftServer/NotificationIcon.cs
@@ -0,0 +1,105 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: albe
+ * Date: 8/16/2016
+ * Time: 1:02 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+using Thrift;
+using Thrift.Server;
+using Thrift.Transport;
+
+namespace ThriftServer
+{
+	public sealed class NotificationIcon
+	{
+		private NotifyIcon notifyIcon;
+		private ContextMenu notificationMenu;
+		private Thread hilo;
+		#region Initialize icon and menu
+		public NotificationIcon()
+		{
+			notifyIcon = new NotifyIcon();
+			notificationMenu = new ContextMenu(InitializeMenu());
+			
+			notifyIcon.DoubleClick += IconDoubleClick;
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NotificationIcon));
+			notifyIcon.Icon = (Icon)resources.GetObject("$this.Icon");
+			notifyIcon.ContextMenu = notificationMenu;
+			hilo =new Thread(new ThreadStart(this.thrift));
+			hilo.IsBackground=true;
+			hilo.Start();
+		}
+		private void thrift(){
+			//Thrift stuff
+			var handler = new OpenFileHandler();
+			var processor = new OpenFileService.Processor(handler);
+			
+			TServerTransport transport = new TServerSocket(9090);
+			TServer server = new TThreadPoolServer(processor, transport);
+			
+			server.Serve();
+			//end thrift
+		}
+		private MenuItem[] InitializeMenu()
+		{
+			MenuItem[] menu = new MenuItem[] {
+				new MenuItem("About", menuAboutClick),
+				new MenuItem("Exit", menuExitClick)
+			};
+			return menu;
+		}
+		#endregion
+		
+		#region Main - Program entry point
+		/// <summary>Program entry point.</summary>
+		/// <param name="args">Command Line Arguments</param>
+		[STAThread]
+		public static void Main(string[] args)
+		{
+			
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			
+			bool isFirstInstance;
+			// Please use a unique name for the mutex to prevent conflicts with other programs
+			using (Mutex mtx = new Mutex(true, "ThriftServer", out isFirstInstance)) {
+				if (isFirstInstance) {
+					NotificationIcon notificationIcon = new NotificationIcon();
+					notificationIcon.notifyIcon.Visible = true;
+					Application.Run();
+					notificationIcon.notifyIcon.Dispose();
+				} else {
+					// The application is already running
+					// TODO: Display message box or change focus to existing application instance
+				}
+			} // releases the Mutex
+			
+		}
+		#endregion
+		
+		#region Event Handlers
+		private void menuAboutClick(object sender, EventArgs e)
+		{
+			MessageBox.Show("C# thrift example server.");
+		}
+		
+		private void menuExitClick(object sender, EventArgs e)
+		{
+			
+			Application.Exit();
+		}
+		
+		private void IconDoubleClick(object sender, EventArgs e)
+		{
+			MessageBox.Show("The icon was double clicked");
+		}
+		#endregion
+	}
+}
---
+++ ThriftServer/OpenFileHandler.cs
@@ -0,0 +1,78 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: albe
+ * Date: 8/16/2016
+ * Time: 1:09 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ThriftServer
+{
+	/// <summary>
+	/// Description of OpenFileHandler.
+	/// </summary>
+	public class OpenFileHandler:OpenFileService.Iface
+	{
+		public FileInf fileToOpen;
+		public OpenFileHandler()
+		{
+		}
+		
+		
+		
+		public bool openFile(FileInf info, string parameters)
+		{
+			
+			System.Diagnostics.Process.Start(info.FileName);
+			return true;
+		}
+		
+		public IAsyncResult Begin_openFile(AsyncCallback callback, object state, FileInf info, string parameters)
+		{
+			throw new NotImplementedException();
+		}
+		
+		public bool End_openFile(IAsyncResult asyncResult)
+		{
+			throw new NotImplementedException();
+		}
+		
+		public List<FileInf> listFolder(string path)
+		{
+			DirectoryInfo di=new DirectoryInfo(path);
+			FileInfo[] files=di.GetFiles();
+			DirectoryInfo[] directories=di.GetDirectories();
+			List<FileInf>aux_list=new List<FileInf>();
+			foreach (DirectoryInfo element in directories) {
+				FileInf aux_file=new FileInf();
+				aux_file.FileName=element.FullName;
+				aux_file.Extension="";
+				aux_file.IsDir=true;
+				aux_list.Add(aux_file);
+			}
+			foreach (FileInfo element in files) {
+				FileInf aux_file=new FileInf();
+				aux_file.FileName=element.FullName;
+				aux_file.Extension=element.Extension.Replace(".","");
+				aux_file.IsDir=false;
+				aux_list.Add(aux_file);
+			}
+			return aux_list;
+		}
+		
+		public IAsyncResult Begin_listFolder(AsyncCallback callback, object state, string path)
+		{
+			throw new NotImplementedException();
+		}
+		
+		public List<FileInf> End_listFolder(IAsyncResult asyncResult)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
